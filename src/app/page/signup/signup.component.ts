ba91007dc89baee2877c250f7dc54ec7e0702d44
campo22 <miguelcampodiaz@gmail.com>
2024-11-06 15:24:23 -0500

    agregando formulario register corrigiendo erroresv2

--- src/app/page/signup/signup.component.ts
+++ src/app/page/signup/signup.component.ts
@@ -1,21 +1,36 @@
+// signup.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSelectModule,
+    MatButtonModule,
   ],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  // Lógica del componente
+  user = {
+    username: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log('Formulario enviado', this.user);
+    // Aquí puedes agregar la lógica para enviar los datos al servidor
+  }
 }
