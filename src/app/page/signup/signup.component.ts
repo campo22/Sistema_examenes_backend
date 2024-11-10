0b5e580e9870b530c0d47e98d4f4958971e056fb
campo22 <miguelcampodiaz@gmail.com>
2024-11-10 10:09:55 -0500

    conectando register con la api

--- src/app/page/signup/signup.component.ts
+++ src/app/page/signup/signup.component.ts
@@ -1,10 +1,11 @@
 // signup.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { UserService } from '../../Services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -21,11 +22,28 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup;
+  private _apiUserService = inject(UserService);
+  registrationSuccess = false;
 
-  constructor(private fb: FormBuilder) {
+
+  public user = {
+    username: '',
+    password: '',
+    name: '',
+    lastname: '',
+    email: '',
+    phone: ''
+
+  }
+
+
+
+
+
+  constructor(private fb: FormBuilder,) {
     this.signupForm = this.fb.group({
       username: ['', Validators.required],
-      firstName: ['', Validators.required],
+      name: ['', Validators.required],
       lastName: ['', Validators.required],
       phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
       email: ['', [Validators.required, Validators.email]],
@@ -33,12 +51,35 @@ export class SignupComponent implements OnInit {
     });
   }
 
+
   ngOnInit(): void { }
 
   onSubmit() {
     if (this.signupForm.valid) {
-      console.log('Formulario enviado', this.signupForm.value);
-      // Aquí puedes agregar la lógica para enviar los datos al servidor
+      console.log(this.signupForm.value);
+
+      // Simulate API call
+      this._apiUserService.posUser(this.signupForm.value).subscribe(
+        (data) => {
+          console.log(data);
+          console.log("Datos enviados de manera correcta");
+          this.registrationSuccess = true;
+        },
+        (error) => {
+          console.log("Error al enviar los datos" + error)
+        }
+      );
+    } else {
+      this.signupForm.markAllAsTouched();
     }
   }
+
+  get username() { return this.signupForm.get('username'); }
+  get name() { return this.signupForm.get('name'); }
+  get lastName() { return this.signupForm.get('lastName'); }
+  get phone() { return this.signupForm.get('phone'); }
+  get email() { return this.signupForm.get('email'); }
+  get password() { return this.signupForm.get('password'); }
+
+
 }
